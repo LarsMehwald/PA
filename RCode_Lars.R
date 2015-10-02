b358a752ba1a8cac939ab7d6d9e321bc210b0c45
LarsMehwald <lars.mehwald@posteo.de>
2015-10-02 13:48:58 +0200

    Update RCode_Lars.R

--- RCode_Lars.R
+++ RCode_Lars.R
@@ -1,1 +1,12 @@
-# First line of code
+#####################
+# Pair Assignment 1
+# Lars Mehwald and Daniel Salgado
+# 10 October 2015
+####################
+
+#Loading needed packages
+library('ggplot2')
+
+#Loading R datasets
+data()
+data(swiss)
